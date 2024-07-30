40f7fe990a7f9ab34e020f55a145c7730dd6d3ba
maugallo <maugallok@gmail.com>
2024-07-29 23:41:39 -0300

    configuration files

--- src/main/java/edu/usal/suravicIntegrity/SuravicIntegrityApplication.java
+++ src/main/java/edu/usal/suravicIntegrity/SuravicIntegrityApplication.java
@@ -2,19 +2,16 @@ package edu.usal.suravicIntegrity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
-
-import java.util.Arrays;
+import org.springframework.core.env.AbstractEnvironment;
 
 @SpringBootApplication
 public class SuravicIntegrityApplication {
 
 	public static void main(String[] args) {
+		if (System.getProperty(AbstractEnvironment.ACTIVE_PROFILES_PROPERTY_NAME) == null)
+			System.setProperty(AbstractEnvironment.ACTIVE_PROFILES_PROPERTY_NAME, "dev");
+
 		SpringApplication.run(SuravicIntegrityApplication.class, args);
-		System.out.println("Server started!");
 	}
 
 }
