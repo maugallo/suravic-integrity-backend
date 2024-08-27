ccd12447b1f69ac482ca0303792a8ac084c69aed
maugallo <maugallok@gmail.com>
2024-08-27 11:20:00 -0300

    Cors Configuration

---
+++ src/main/java/edu/usal/suravicIntegrity/config/CorsConfiguration.java
@@ -0,0 +1,25 @@
+package edu.usal.suravicIntegrity.config;
+
+import lombok.NonNull;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfiguration {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:8100")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowCredentials(true);
+            }
+        };
+    }
+
+}
